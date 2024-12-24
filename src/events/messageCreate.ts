cf4bd39c54d57c31abe092b2926e294286cf9251
minagishl <mvxd568.co@icloud.com>
2024-12-24 22:06:04 +0900

    first commit

---
+++ src/events/messageCreate.ts
@@ -0,0 +1,87 @@
+import {
+  Events,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+  type Message,
+} from "discord.js";
+
+// Check if the URL is included
+function containsURL(input: string): boolean {
+  // URL regular expression
+  const urlRegex =
+    /https?:\/\/(?:www\.|[a-zA-Z0-9-]+\.)[a-zA-Z0-9.-]+(?:\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)?/g;
+
+  // Determine if a regular expression is matched
+  return urlRegex.test(input);
+}
+
+export default {
+  name: Events.MessageCreate,
+  async execute(message: Message): Promise<void> {
+    if (message.author.bot || !message.inGuild()) return;
+
+    if (containsURL(message.content)) {
+      const content = message.content;
+
+      const ok = new ButtonBuilder()
+        .setCustomId("send")
+        .setLabel("SEND")
+        .setStyle(ButtonStyle.Secondary);
+
+      const ng = new ButtonBuilder()
+        .setCustomId("cancel")
+        .setLabel("CANCEL")
+        .setStyle(ButtonStyle.Secondary);
+
+      const row = new ActionRowBuilder<ButtonBuilder>().addComponents(ok, ng);
+
+      const reply = await message.reply({
+        components: [row.toJSON()],
+      });
+
+      message.delete();
+
+      interface InteractionFilter {
+        user: {
+          id: string;
+        };
+      }
+
+      const filter = (interaction: InteractionFilter): boolean => {
+        return interaction.user.id === message.author.id;
+      };
+
+      const collector = reply.createMessageComponentCollector({
+        filter,
+        time: 15000,
+      });
+
+      collector.on("collect", async (interaction) => {
+        if (interaction.customId === "send") {
+          await message.channel.send(
+            content + `\n\nThis message was posted by <@${message.author.id}>`,
+          );
+          await interaction.update({
+            components: [],
+            content: "The message has been sent.",
+          });
+        } else {
+          await interaction.update({
+            components: [],
+            content: "The message has been canceled.",
+          });
+
+          await interaction.followUp({
+            content: content,
+            ephemeral: true,
+          });
+        }
+      });
+
+      collector.on("end", async () => {
+        await reply.delete();
+      });
+    }
+  },
+};
